ebff53e302db4d602d0bff3a0f34eb47e720f632
KAMRUZZAMAN LEION <leion.kamruzzaman@brilliant.com.bd>
2024-04-22 15:44:10 +0600

    added useMemo and useCallback hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useMemo, useState } from "react";
+import Title from "./components/Title";
+import ShowCount from "./components/ShowCount";
+import Button from "./components/Button";
 
 function App() {
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const incrementOne = useCallback(() => {
+    setCount1((prevCount) => prevCount + 1);
+  }, []);
+
+  const incrementFive = useCallback(() => {
+    setCount2((prevCount) => prevCount + 5);
+  }, []);
+
+  const isEvenOrOdd = useMemo(() => {
+    let i = 0;
+    while (i < 10000000) {
+      i++;
+    }
+    return count1 % 2 === 0;
+  }, [count1]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title />
+      <ShowCount count={count1} title="Counter 1" />
+      <span>{isEvenOrOdd ? "Even" : "Odd"}</span>
+      <Button handleClick={incrementOne}>Increment by one</Button>
+      <hr />
+      <ShowCount count={count2} title="Counter 2" />
+      <Button handleClick={incrementFive}>Increment by Five</Button>
     </div>
   );
 }
